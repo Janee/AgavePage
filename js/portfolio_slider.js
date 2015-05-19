ac0546a79a6165378976743d1993eabf490ed231
Diana Enriquez <diana@agaveti.com>
2015-05-19 17:55:01 -0500

    Nuevas imagenes a portafolio

--- js/portfolio_slider.js
+++ js/portfolio_slider.js
@@ -13,9 +13,8 @@ $("#elastic_grid_demo").elastic_grid({
 			'thumbnail' : ['img/portfolio/small/msb/1.jpg', 'img/portfolio/small/msb/2.jpg', 'img/portfolio/small/msb/3.jpg', 'img/portfolio/small/msb/4.jpg'],
 			'large' : ['img/portfolio/large/msb/1.jpg', 'img/portfolio/large/msb/2.jpg', 'img/portfolio/large/msb/3.jpg', 'img/portfolio/large/msb/4.jpg'],
 			'button_list'   :
-			[/**
-			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}*/
+			[
+			{ 'title':'Website', 'url':'http://www.mysomethingblue.com.mx'}
 			],
 			'tags'  : ['Todos', 'Website']
 			},
@@ -34,28 +33,28 @@ $("#elastic_grid_demo").elastic_grid({
 			},
 
 			{
-			'title' : 'Spinach winter purslane',
-			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-			'thumbnail' : ['img/portfolio/small/15.jpg','img/portfolio/small/8.jpg', 'img/portfolio/small/9.jpg', 'img/portfolio/small/10.jpg'],
-			'large' : ['img/portfolio/large/15.jpg','img/portfolio/large/8.jpg', 'img/portfolio/large/9.jpg', 'img/portfolio/large/10.jpg'],
+			'title' : 'Agua Artesana',
+			'description'   : 'El arte de sanar. Empresa generadora de Agua potable.',
+			'thumbnail' : ['img/portfolio/small/ar/1.png','img/portfolio/small/ar/2.png'],
+			'large' : ['img/portfolio/large/ar/1.png','img/portfolio/large/ar/2.png'],
 			'button_list'   :
 			[/**
-			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}*/
+			{ 'title':'Demo', 'url' : 'http://#' },*/
+			{ 'title':'Website', 'url':'http://www.aguartesana.com'}
 			],
-			'tags'  : ['Website', 'Todos']
+			'tags'  : ['Website', 'Diseño', 'Todos']
 			},
 
 			{
-			'title' : 'Aubergine napa cabbage',
-			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-			'thumbnail' : ['img/portfolio/small/12.jpg', 'img/portfolio/small/13.jpg', 'img/portfolio/small/14.jpg', 'img/portfolio/small/15.jpg', 'img/portfolio/small/16.jpg'],
-			'large' : ['img/portfolio/large/12.jpg', 'img/portfolio/large/13.jpg', 'img/portfolio/large/14.jpg', 'img/portfolio/large/15.jpg', 'img/portfolio/large/16.jpg'],
+			'title' : 'Pasarela Miss XV NL',
+			'description'   : 'Certamen y talleres enfocados a jóvenes adolescentes.',
+			'thumbnail' : ['img/portfolio/small/pm/1.png', 'img/portfolio/small/pm/2.png', 'img/portfolio/small/pm/3.png'],
+			'large' : ['img/portfolio/large/pm/1.png', 'img/portfolio/large/pm/2.png', 'img/portfolio/large/pm/3.png'],
 			'button_list'   :
 			[
-			{ 'title':'Descargar', 'url':'http://#'}
+			{ 'title':'Website', 'url':'http://pasarelamissxv.nl'}
 			],
-			'tags'  : ['App','Todos']
+			'tags'  : ['Website','FSW', 'Diseño','Todos']
 			},
 
 			{
@@ -109,7 +108,7 @@ $("#elastic_grid_demo").elastic_grid({
 			'tags'  : ['App', 'Website', 'FSW', 'Todos']
 			},
 
-			{
+			/*{
 			'title' : 'Swiss chard pumpkin',
 			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
 			'thumbnail' : ['img/portfolio/small/18.jpg', 'img/portfolio/small/18.jpg', 'img/portfolio/small/19.jpg', 'img/portfolio/small/20.jpg'],
@@ -117,7 +116,7 @@ $("#elastic_grid_demo").elastic_grid({
 			'button_list'   :
 			[/**
 			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}*/
+			{ 'title':'Download', 'url':'http://#'}
 			],
 			'tags'  : ['FSW','Todos']
 			},
@@ -130,7 +129,7 @@ $("#elastic_grid_demo").elastic_grid({
 			'button_list'   :
 			[/**
 			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}*/
+			{ 'title':'Download', 'url':'http://#'}
 			],
 			'tags'  : ['Website','Todos']
 			},
@@ -143,7 +142,7 @@ $("#elastic_grid_demo").elastic_grid({
 			'button_list'   :
 			[/**
 			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}*/
+			{ 'title':'Download', 'url':'http://#'}
 			],
 			'tags'  : ['Diseño','Todos']
 			},
@@ -158,7 +157,7 @@ $("#elastic_grid_demo").elastic_grid({
 			{ 'title':'Descargar', 'url':'http://#'}
 			],
 			'tags'  : ['App', 'FSW','Todos']
-			}
+			}*/
 		]
 	});
 });
